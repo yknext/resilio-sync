2b15ed0dce686b612f76ea603e6a3a9fee6fb6f7
yknext <yangkang2014@gmail.com>
2017-08-19 16:37:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 # for fuck gfw
 iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
+iptables -I INPUT -p tcp --tcp-flags ALL RST,ACK -j DROP
+iptables -I OUTPUT -p tcp --tcp-flags ALL RST,ACK -j DROP
 
 mkdir -p /mnt/sync/folders
 mkdir -p /mnt/sync/config
